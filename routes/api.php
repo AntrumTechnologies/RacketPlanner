f4f7821c3a122a0cc30f00396810a6a04f233f50
Patrick van Broeckhuijsen <github@patb.ro>
2023-06-28 11:48:15 +0000

    Update API routes to match controllers

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CourtController;
+use App\Http\Controllers\MatchDetailsController;
+use App\Http\Controllers\TournamentController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +19,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Auth::routes(); // TODO: check whether routes should/can go through Sanctum instead
+
+Route::get('/court', [CourtController::class, 'index']);
+Route::get('/court/{id}', [CourtController::class, 'show']);
+Route::post('/court/create', [CourtController::class, 'store']);
+Route::post('/court', [CourtController::class, 'update']);
+Route::post('/court/delete', [CourtController::class, 'delete']);
+
+Route::get('/match/{id}', [MatchDetailsController::class, 'show']);
+Route::post('/match/create', [MatchDetailsController::class, 'store']);
+Route::post('/match', [MatchDetailsController::class, 'update']);
+Route::post('/match/delete', [MatchDetailsController::class, 'delete']);
+
+Route::get('/tournament', [TournamentController::class, 'index']);
+Route::get('/tournament/{id}', [TournamentController::class, 'show']);
+Route::get('/tournament/{id}/matches', [TournamentController::class, 'showMatches']);
+Route::post('/tournament/create', [TournamentController::class, 'store']);
+Route::post('/tournament/court', [TournamentController::class, 'matchCourts']);
+Route::post('/tournament/user', [TournamentController::class, 'matchUsers']);
+Route::post('/tournament', [TournamentController::class, 'update']);
+Route::post('/tournament/delete', [TournamentController::class, 'delete']);
+
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+Route::post('/user', [UserController::class, 'update']);
