201acd6f96b344ae2bf5f3411134462f2539d395
Patrick van Broeckhuijsen <github@patb.ro>
2023-07-15 21:00:41 +0200

    Add magic link

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Notifications\MagicEmail;
+use Illuminate\Http\Request;
+use Illuminate\Notifications\Notifiable;
+use MagicLink\Actions\LoginAction;
+use MagicLink\MagicLink;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required|exists:users',
+        ]);
+
+        $user = User::where('email', $request->get('email'))->first();
+        $urlToAutoLogin =  MagicLink::create(new LoginAction($user))->url;
+        
+        $array = [
+            'name' => $user->name,
+            'email' => $user->email,
+            'url' => $urlToAutoLogin,
+        ];
+
+        $user->notify(new MagicEmail($array));
+
+        return view("auth.verify", ['email' => $user->email]);
+    }
+}
