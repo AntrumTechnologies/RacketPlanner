39bf359c7003aec2d0876b8a90d91db27386cf37
Patrick van Broeckhuijsen <github@patb.ro>
2023-08-06 08:32:05 +0200

    Divide DatabaseController content over multiple files

--- app/Http/Controllers/Planner/MatchDetailsController.php
+++ app/Http/Controllers/Planner/MatchDetailsController.php
@@ -6,38 +6,89 @@ use App\Models\MatchDetails;
 
 class MatchDetailsController extends PlannerController
 {
-    public function show($id) {
-        $match = MatchDetails::findOrFail($id);
-        return $match;
+    private $tournamentId;
+
+    public function __construct($tournamentId)
+    {
+        $this->middleware('auth');
+        $this->tournamentId = $tournamentId;
     }
 
-    public function store($tournament_id, $player1a_id, $player1b_id, $player2a_id, $player2b_id, $rating) {
-        $newMatchDetails = new MatchDetails([
-            'tournament_id' => $tournament_id,
-            'player1a_id' => $player1a_id,
-            'player1b_id' => $player1b_id,
-            'player2a_id' => $player2a_id,
-            'player2b_id' => $player2b_id,
-            'rating' => $rating,
-            'created_by' => Auth::id(),
+    public function GetMatches() {
+        Report::Trace(__METHOD__);
+
+        return MatchDetails::where('tournament_id', $tournamentId)->get();
+    }
+
+    public function GetMatchById($matchId) {
+        Report::Trace(__METHOD__);
+
+        return MatchDetails::where('id', $matchId)->where('tournament_id', $tournamentId);
+    }
+
+    public function InsertMatch($player1aId, $player1bId, $player2aId, $player2bId, $rating, $ratingDiff) {
+        Report::Trace(__METHOD__);
+
+        $newMatch = new MatchDetails([
+            "tournament_id" => $this->tournamentId,
+            "player1a_id" => $player1aId,
+            "player1b_id" => $player1bId,
+            "player2a_id" => $player2aId,
+            "player2b_id" => $player2bId,
+            "rating" => $rating,
+            "rating_diff" => $ratingDiff,
         ]);
 
-        $newMatchDetails->save();
+        return $newMatch->save();
     }
 
-    public function enable_all_matches($tournament_id) {
+    public function EnableAllMatches() {
+        Report::Trace(__METHOD__);
+
         MatchDetails::where('tournament_id', $tournament_id)->update(['disabled' => 0]);
     }
 
-    public function disable_matches_for_player($tournament_id, $player_id) {
-        MatchDetails::where(['tournament_id', '=', $tournament_id], ['player1a_id', '=', $player_id])
-            ->orWhere(['tournament_id', '=', $tournament_id], ['player1b_id', '=', $player_id])
-            ->orWhere(['tournament_id', '=', $tournament_id], ['player2a_id', '=', $player_id])
-            ->orWhere(['tournament_id', '=', $tournament_id], ['player2b_id', '=', $player_id])
+    public function DisableMatchById($matchId) {
+        Report::Trace(__METHOD__);
+
+        return MatchDetails::where(['tournament_id', '=', $this->tournamentId], ['id', '=', $matchId])->update(['disabled' => 1]);
+    }
+
+    public function DisableMatchesForPlayer($playerId) {
+        Report::Trace(__METHOD__);
+
+        return MatchDetails::where(['tournament_id', '=', $this->tournamentId], ['player1a_id', '=', $playerId])
+            ->orWhere(['tournament_id', '=', $this->tournamentId], ['player1b_id', '=', $playerId])
+            ->orWhere(['tournament_id', '=', $this->tournamentId], ['player2a_id', '=', $playerId])
+            ->orWhere(['tournament_id', '=', $this->tournamentId], ['player2b_id', '=', $playerId])
             ->update(['disabled' => 1]);
     }
 
-    public function get_available_matches($tournament_id) {
-        MatchDetails::where('tournament_id', $tournament_id)->where('disabled', 0)->get();
+    public function GetAvailableMatches() {
+        Report::Trace(__METHOD__);
+
+        return MatchDetails::where('tournament_id', $this->tournamentId)->where('disabled', 0)->get();
+    }
+
+    public function SetPriorityForMatch($matchId, $priority) {
+        Report::Trace(__METHOD__);
+
+        return MatchDetails::where('id', $matchId)->update(['priority' => $priority]);;
+    }
+
+    public function GetBestMatch() {
+        Report::Trace(__METHOD__);
+
+        $matches = MatchDetails::where('tournament_id', $this->tournamentId)
+            ->where('disabled', '0')
+            ->orderByAsc('priority')
+            ->orderByDesc('rating')
+            ->limit(1)
+            ->get();
+
+        if ($matches->count() > 0)
+            return $matches->first();
+
+        return null;
     }
 }
--- app/Http/Controllers/Planner/ScheduleController.php
+++ app/Http/Controllers/Planner/ScheduleController.php
@@ -8,38 +8,96 @@ use App\Models\Match;
 
 class ScheduleController extends PlannerController
 {
-    public function show($tournament_id) {
-        $schedule = Schedule::where('tournament_id', $tournament_id)->get();
-        return $schedule;
+    public function GetSchedule() {
+        Report::Trace(__METHOD__);
+
+        return Schedule::where('tournament_id', $this->tournamentId);;
     }
 
-    public function generate_schedule($tournament_id) {
-        // TODO(PATBRO): prevent generating schedule twice?
-        $rounds = Round::where('tournament_id', $tournament_id)->get();
-        $courts = Court::where('tournament_id', $tournament_id)->get();
+    public function DeleteSchedule() {
+        Report::Trace(__METHOD__);
+
+        $schedule = Schedule::where('tournament_id', $this->tournamentId);
+        $schedule->delete();
+    }
+
+    public function GenerateSchedule() {
+        Report::Trace(__METHOD__);
+
+        $rounds = Round::where('tournament_id', $this->tournamentId)->get();
+        $courts = Court::where('tournament_id', $this->tournamentId)->get();
 
         foreach ($rounds as $round) {
             foreach ($courts as $court) {
-                $newSchedule = new Schedule ([
-                    'tournament_id' => $tournament_id,
-                    'court_id' => $court->id,
-                    'round_id' => $round->id,
+                $newSlot = new Schedule([
+                    "tournament_id" => $this->tournamentId,
+                    "court_id" => $court->id,
+                    "round_id" => $round->id,
                 ]);
 
-                $newSchedule->save();
+                if (!$newSlot->save()) {
+                    Report::Fail("Unable to add slot ('$court->id', '$round->id') to schedule.");
+                    return false;
+                }
             }
         }
+
+        return true;
     }
 
-    public function set_slot_state($slot_id, $state) {
-        $schedule = Schedule::where('id', $slot_id)->update(['match_id' => null, 'state' => $state]);
+    public function SetSlotStateToClinic($slotId) {
+        Report::Trace(__METHOD__);
+
+        return Schedule::where('id', $slotId)
+            ->update([
+                'state' => 'clinic',
+                'match_id' => null,
+            ]);
     }
 
-    public function assign_match_to_slot($slot_id, $match_id) {
-        $schedule = Schedule::where('id', $slot_id)->update(['match_id' => $match_id, 'state' => 'available']);
+    public function SetSlotStateToDisabled($slotId) {
+        Report::Trace(__METHOD__);
+
+        return Schedule::where('id', $slotId)
+            ->update([
+                'state' => 'disabled',
+                'match_id' => null,
+            ]);
     }
 
-    public function delete_match_to_slot($slot_id, $match_id) {
-        $schedule = Schedule::where('id', $slot_id)->update(['match_id' => null, 'state' => 'available']);
+    public function SetSlotStateToAvailable($slotId) {
+        Report::Trace(__METHOD__);
+
+        return Schedule::where('id', $slotId)
+            ->update([
+                'state' => 'available',
+                'match_id' => null,
+            ]);
+    }
+
+    public function AssignMatchToSlot($slotId, $matchId) {
+        Report::Trace(__METHOD__);
+
+        return Schedule::where('id', $slotId)
+            ->update([
+                'state' => 'available',
+                'match_id' => $matchId,
+            ]);
+    }
+
+    public function DeleteMatchFromSlot($slotId) {
+        Report::Trace(__METHOD__);
+
+        return Schedule::where('id', $slotId)
+            ->update([
+                'state' => 'available',
+                'match_id' => null,
+            ]);
+    }
+
+    public function GetSlotsByRoundId($roundId) {
+        Report::Trace(__METHOD__);
+
+        return Schedule::where('tournament_id', $this->tournamentId)->where('round_id', $roundId)->get();
     }
 }
