13f313959fc277561221ada2287120363fcef6fe
Patrick van Broeckhuijsen <github@patb.ro>
2023-08-10 22:26:28 +0200

    Add breadcrumbs to match details

--- resources/views/match.blade.php
+++ resources/views/match.blade.php
@@ -4,13 +4,20 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h2>{{ $match->name }}</h2>
+            <h2>Match Details</h2>
+
+            <nav aria-label="breadcrumb" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('tournament', $match->tournament_id) }}">Tournament</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Match details</li>
+                </ol>
+            </nav>
         </div>
     </div>
 
     <div class="row justify-content-center mt-4">
         <div class="col-md-8">
-            <h4>Match Details</h4>
+            <h4>{{ $match->tournament_name }}</h4>
 
             <div class="card mb-4">
                 <div class="card-header d-flex">
