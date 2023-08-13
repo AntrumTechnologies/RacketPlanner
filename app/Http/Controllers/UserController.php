459bf37e6734c0b21c9663ddb0532da7a8c25d27
Patrick van Broeckhuijsen <github@patb.ro>
2023-08-13 12:02:56 +0200

    Show clinic(s) on User view as well, and re-use user matches view layout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\Tournament;
 use App\Models\TournamentUser;
 use App\Models\MatchDetails;
 use App\Models\Player;
+use App\Models\Schedule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -30,87 +31,140 @@ class UserController extends Controller
     public function show($id) {
         $user = User::findOrFail($id);
 
-        $tournaments = Player::where('user_id', $id)->join('tournaments', 'tournaments.id', '=', 'players.tournament_id')->get();
+        $user_tournaments = Player::where('user_id', $id)->join('tournaments', 'tournaments.id', '=', 'players.tournament_id')->get();
 
-        if (Auth::user()->can('admin')) {
-            $matches = DB::select("SELECT 
-                        rounds.starttime as 'time',
-                        courts.name as 'court',
-                        user1a.name as `player1a`,
-                        user1a.id as `player1a_id`,
-                        user1a.avatar as `player1a_avatar`,
-                        user1b.name as `player1b`,
-                        user1b.id as `player1b_id`,
-                        user1b.avatar as `player1b_avatar`,
-                        user2a.name as `player2a`,
-                        user2a.id as `player2a_id`,
-                        user2a.avatar as `player2a_avatar`,
-                        user2b.name as `player2b`,
-                        user2b.id as `player2b_id`,
-                        user2b.avatar as `player2b_avatar`,
-                        matches.id,
-                        matches.score1,
-                        matches.score2
-                    FROM `schedules`
-                        INNER JOIN `rounds` ON schedules.round_id = rounds.id
-                        INNER JOIN `courts` ON schedules.court_id = courts.id
-                        INNER JOIN `matches` ON schedules.match_id = matches.id
-                        INNER JOIN `players` as player1a ON matches.player1a_id = player1a.id
-                        INNER JOIN `players` as player1b ON matches.player1b_id = player1b.id
-                        INNER JOIN `players` as player2a ON matches.player2a_id = player2a.id
-                        INNER JOIN `players` as player2b ON matches.player2b_id = player2b.id
-                        INNER JOIN `users` as user1a ON player1a.user_id = user1a.id
-                        INNER JOIN `users` as user1b ON player1b.user_id = user1b.id
-                        INNER JOIN `users` as user2a ON player2a.user_id = user2a.id
-                        INNER JOIN `users` as user2b ON player2b.user_id = user2b.id
-                    WHERE (player1a.user_id = ". $id ." 
-                        OR player1b.user_id = ". $id ." 
-                        OR player2a.user_id = ". $id ." 
-                        OR player2b.user_id = ". $id .")
-                    ORDER BY time ASC");
-        } else {
-            $matches = DB::select("SELECT 
-                        rounds.starttime as 'time',
-                        courts.name as 'court',
-                        user1a.name as `player1a`,
-                        user1a.id as `player1a_id`,
-                        user1a.avatar as `player1a_avatar`,
-                        user1b.name as `player1b`,
-                        user1b.id as `player1b_id`,
-                        user1b.avatar as `player1b_avatar`,
-                        user2a.name as `player2a`,
-                        user2a.id as `player2a_id`,
-                        user2a.avatar as `player2a_avatar`,
-                        user2b.name as `player2b`,
-                        user2b.id as `player2b_id`,
-                        user2b.avatar as `player2b_avatar`,
-                        matches.id,
-                        matches.score1,
-                        matches.score2
-                    FROM `schedules`
-                        INNER JOIN `rounds` ON schedules.round_id = rounds.id
-                        INNER JOIN `courts` ON schedules.court_id = courts.id
-                        INNER JOIN `matches` ON schedules.match_id = matches.id
-                        INNER JOIN `players` as player1a ON matches.player1a_id = player1a.id
-                        INNER JOIN `players` as player1b ON matches.player1b_id = player1b.id
-                        INNER JOIN `players` as player2a ON matches.player2a_id = player2a.id
-                        INNER JOIN `players` as player2b ON matches.player2b_id = player2b.id
-                        INNER JOIN `users` as user1a ON player1a.user_id = user1a.id
-                        INNER JOIN `users` as user1b ON player1b.user_id = user1b.id
-                        INNER JOIN `users` as user2a ON player2a.user_id = user2a.id
-                        INNER JOIN `users` as user2b ON player2b.user_id = user2b.id
-                    WHERE (player1a.user_id = ". $id ." 
-                        OR player1b.user_id = ". $id ." 
-                        OR player2a.user_id = ". $id ." 
-                        OR player2b.user_id = ". $id .") AND schedules.public = 1
-                    ORDER BY time ASC");
+        $user_clinics = array();
+        if (Player::where('user_id', $user->id)->where('clinic', 1)->count() > 0) {
+            foreach ($user_tournaments as $player) {
+                if (Auth::user()->can('admin')) {
+                    $clinics = Schedule::where('schedules.tournament_id', $player->tournament_id)
+                        ->where('schedules.state', 'clinic')
+                        ->join('rounds', 'rounds.id', '=', 'schedules.round_id')
+                        ->join('courts', 'courts.id', '=', 'schedules.court_id')
+                        ->select(
+                            'courts.name as court',
+                            'rounds.starttime as time')
+                        ->get();
+                } else {
+                    $clinics = Schedule::where('schedules.tournament_id', $player->tournament_id)
+                        ->where('schedules.state', 'clinic')
+                        ->where('schedules.public', 1)
+                        ->join('rounds', 'rounds.id', '=', 'schedules.round_id')
+                        ->join('courts', 'courts.id', '=', 'schedules.court_id')
+                        ->select(
+                            'courts.name as court',
+                            'rounds.starttime as time')
+                        ->get();
+                }
+                
+                foreach ($clinics as $clinic) {
+                    $clinic->time = date('H:i', strtotime($clinic->time));
+
+                    $clinic->players = Player::where('tournament_id', $player->tournament_id)
+                        ->where('players.clinic', 1)
+                        ->join('users', 'users.id', '=', 'players.user_id')
+                        ->select(
+                            'users.name as user_name',
+                            'users.id as user_id')
+                        ->get();
+
+                    $user_clinics[] = $clinic;
+                }
+            }   
         }
 
-        foreach ($matches as $match) {
-            $match->time = date('H:i', strtotime($match->time));
-        }   
+        $user_matches_per_tournament = array();
+        foreach ($user_tournaments as $player) {
+            if (Auth::user()->can('admin')) {
+                $matches = DB::select("SELECT 
+                        rounds.starttime as 'time',
+                        courts.name as 'court',
+                        user1a.name as `player1a`,
+                        user1a.id as `player1a_id`,
+                        user1a.avatar as `player1a_avatar`,
+                        user1b.name as `player1b`,
+                        user1b.id as `player1b_id`,
+                        user1b.avatar as `player1b_avatar`,
+                        user2a.name as `player2a`,
+                        user2a.id as `player2a_id`,
+                        user2a.avatar as `player2a_avatar`,
+                        user2b.name as `player2b`,
+                        user2b.id as `player2b_id`,
+                        user2b.avatar as `player2b_avatar`,
+                        matches.id,
+                        matches.score1,
+                        matches.score2
+                    FROM `schedules`
+                        INNER JOIN `rounds` ON schedules.round_id = rounds.id
+                        INNER JOIN `courts` ON schedules.court_id = courts.id
+                        INNER JOIN `matches` ON schedules.match_id = matches.id
+                        INNER JOIN `players` as player1a ON matches.player1a_id = player1a.id
+                        INNER JOIN `players` as player1b ON matches.player1b_id = player1b.id
+                        INNER JOIN `players` as player2a ON matches.player2a_id = player2a.id
+                        INNER JOIN `players` as player2b ON matches.player2b_id = player2b.id
+                        INNER JOIN `users` as user1a ON player1a.user_id = user1a.id
+                        INNER JOIN `users` as user1b ON player1b.user_id = user1b.id
+                        INNER JOIN `users` as user2a ON player2a.user_id = user2a.id
+                        INNER JOIN `users` as user2b ON player2b.user_id = user2b.id
+                    WHERE schedules.tournament_id = ". $player->tournament_id ." AND 
+                        (player1a.id = ". $player->id ." 
+                            OR player1b.id = ". $player->id ." 
+                            OR player2a.id = ". $player->id ." 
+                            OR player2b.id = ". $player->id .")
+                    ORDER BY time ASC");
+            } else {
+                $matches = DB::select("SELECT 
+                        rounds.starttime as 'time',
+                        courts.name as 'court',
+                        user1a.name as `player1a`,
+                        user1a.id as `player1a_id`,
+                        user1a.avatar as `player1a_avatar`,
+                        user1b.name as `player1b`,
+                        user1b.id as `player1b_id`,
+                        user1b.avatar as `player1b_avatar`,
+                        user2a.name as `player2a`,
+                        user2a.id as `player2a_id`,
+                        user2a.avatar as `player2a_avatar`,
+                        user2b.name as `player2b`,
+                        user2b.id as `player2b_id`,
+                        user2b.avatar as `player2b_avatar`,
+                        matches.id,
+                        matches.score1,
+                        matches.score2
+                    FROM `schedules`
+                        INNER JOIN `rounds` ON schedules.round_id = rounds.id
+                        INNER JOIN `courts` ON schedules.court_id = courts.id
+                        INNER JOIN `matches` ON schedules.match_id = matches.id
+                        INNER JOIN `players` as player1a ON matches.player1a_id = player1a.id
+                        INNER JOIN `players` as player1b ON matches.player1b_id = player1b.id
+                        INNER JOIN `players` as player2a ON matches.player2a_id = player2a.id
+                        INNER JOIN `players` as player2b ON matches.player2b_id = player2b.id
+                        INNER JOIN `users` as user1a ON player1a.user_id = user1a.id
+                        INNER JOIN `users` as user1b ON player1b.user_id = user1b.id
+                        INNER JOIN `users` as user2a ON player2a.user_id = user2a.id
+                        INNER JOIN `users` as user2b ON player2b.user_id = user2b.id
+                    WHERE schedules.tournament_id = ". $player->tournament_id ." AND 
+                        schedules.public = 1 AND
+                        (player1a.id = ". $player->id ." 
+                            OR player1b.id = ". $player->id ." 
+                            OR player2a.id = ". $player->id ." 
+                            OR player2b.id = ". $player->id .")
+                    ORDER BY time ASC");
+            }
 
-        return view('user', ['user' => $user, 'tournaments' => $tournaments, 'matches' => $matches]);
+            foreach($matches as $match) {
+                $match->time = date('H:i', strtotime($match->time));
+            }
+
+            $user_matches_per_tournament[] = $matches;
+        }
+
+        return view('user', [
+            'user' => $user, 
+            'user_tournaments' => $user_tournaments, 
+            'user_clinics' => $user_clinics,
+            'user_matches_per_tournament' => $user_matches_per_tournament
+        ]);
     }
 
     public function update(Request $request) {
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -13,70 +13,11 @@
         <div class="col-md-8">
             <h4>Scheduled Matches</h4>
 
-            @if (count($matches) == 0)
-            <p>No matches have been scheduled for this user yet.</p>
+            @if (!isset($user_matches_per_tournament[0]) || count($user_matches_per_tournament[0]) == 0)
+                <p>No matches have been scheduled for this user yet.</p>
+            @else
+                @include('layouts.user-matches')
             @endif
-
-            @foreach ($matches as $match)
-                <div class="card mb-4">
-                    <div class="card-header d-flex">
-                        <div class="me-auto" style="font-size: 1.2em">
-                            {{ $match->time }} @ {{ $match->court }}
-                        </div>
-                        <div class="ms-auto">
-                            <a href="{{ route('match', $match->id) }}"><i class="bi bi-link-45deg" style="font-size: 1rem;"></i></a>
-                        </div>
-                    </div>
-
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-9">
-                                <img src="/{{ $match->player1a_avatar }}" class="avatar-sm" />
-                                <a href="{{ route('user', $match->player1a_id) }}" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">{{ $match->player1a }}</a>
-                                <br />
-                                <img src="/{{ $match->player1b_avatar }}" class="avatar-sm mt-2" />
-                                <a href="{{ route('user', $match->player1b_id) }}" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">{{ $match->player1b }}</a>
-                            </div>
-                            <div class="col-3 justify-content-center align-self-center">
-                                @if ($match->score1 != "")
-                                    {{ $match->score1 }}
-                                    @php
-                                        if ($match->score1 > $match->score2) {
-                                    @endphp
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-trophy-fill" viewBox="0 0 16 16"><path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/></svg>
-                                    @php
-                                        }
-                                    @endphp
-                                @endif
-                            </div>
-                        </div>
-
-                        <hr/>
-
-                        <div class="row">
-                            <div class="col-9">
-                                <img src="/{{ $match->player2a_avatar }}" class="avatar-sm" />
-                                <a href="{{ route('user', $match->player2a_id) }}" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">{{ $match->player2a }}</a>
-                                <br />
-                                <img src="/{{ $match->player2b_avatar }}" class="avatar-sm mt-2" />
-                                <a href="{{ route('user', $match->player2b_id) }}" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">{{ $match->player2b }}</a>
-                            </div>
-                            <div class="col-3 justify-content-center align-self-center">
-                                @if ($match->score2 != "")
-                                    {{ $match->score2 }}
-                                    @php
-                                        if ($match->score2 > $match->score1) {
-                                    @endphp
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-trophy-fill" viewBox="0 0 16 16"><path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/></svg>
-                                    @php
-                                        }
-                                    @endphp
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
         </div>
     
         <div class="col-md-8 mt-4">
