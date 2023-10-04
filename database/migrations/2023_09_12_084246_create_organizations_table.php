625e3293fedb76463d6443f47590901926995385
Patrick van Broeckhuijsen <github@patb.ro>
2023-10-04 20:36:39 +0200

    Update organization seeds

--- database/migrations/2023_09_12_084246_create_organizations_table.php
+++ database/migrations/2023_09_12_084246_create_organizations_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('owner_user_id')->unsigned();
             $table->string('location')->nullable();
             $table->timestamps();
         });
--- database/seeders/AdminsOrganizationAssignmentTableSeeder.php
+++ database/seeders/AdminsOrganizationAssignmentTableSeeder.php
@@ -2,23 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Models\Organization;
+use App\Models\AdminOrganizationalAssignment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class OrganizationsTableSeeder extends Seeder
+class AdminsOrganizationalAssignmentTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('organizations')->truncate();
+        DB::table('admins_organizational_assignment')->truncate();
 
         Organization::create([
-            "name" => "Pied Piper",
-            "location" => "Palo Alto",
+            "organization_id" => 1,
+            "user_id" => 1,
         ]);
     }
 }
