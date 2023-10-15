b28daf137b0623cee4b54e54afdeb0f29f26bcd2
Patrick van Broeckhuijsen <github@patb.ro>
2023-10-15 12:56:26 +0200

    Add contact info to organizations' table

--- database/migrations/2023_09_12_084246_create_organizations_table.php
+++ database/migrations/2023_09_12_084246_create_organizations_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('contact_person');
+            $table->string('contact_email');
             $table->string('location')->nullable();
             $table->timestamps();
         });
