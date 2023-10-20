6c6a83196a017f16b50a3cbae7839bd4f71cb22d
Patrick van Broeckhuijsen <github@patb.ro>
2023-10-20 19:23:10 +0200

    Registration implementation

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Tournament;
 use App\Notifications\MagicEmail;
 use Illuminate\Http\Request;
 use Illuminate\Notifications\Notifiable;
@@ -35,5 +36,39 @@ class AuthController extends Controller
     public function logout(Request $request) {
         Auth::logout();
         return redirect('/');
-      }
+    }
+
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'tournament_id' => 'required',
+        ]);
+
+        $user = new User([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            // TODO: add rating
+        ]);
+
+        $user->save();
+
+        if (!empty($request->get('tournament_id'))) {
+            // TODO: security vulnerability! Verify magic link of invite link before logging in user
+            Auth::login($user);
+
+            $tournament = Tournament::findOrFail($request->get('tournament_id'));
+            return redirect()->route('enroll-tournament', ['tournament_id' => $tournament->id]);
+        } else {
+            $magicUrl =  MagicLink::create(new LoginAction($user))->url;
+            $array = [
+                'name' => $user->name,
+                'email' => $user->email,
+                'url' => $magicUrl,
+            ];
+
+            $user->notify(new MagicEmail($array));
+            return view("auth.verify", ['email' => $user->email]);
+        }
+    }
 }
