731d49b02b976d449e8d8d5c988530a2023620e7
Patrick van Broeckhuijsen <github@patb.ro>
2023-11-19 19:49:46 +0100

    Improve lay-out of players view

--- resources/views/admin/players.blade.php
+++ resources/views/admin/players.blade.php
@@ -1,19 +1,25 @@
-@extends('layouts.app', ['title' => 'Players '. $tournament->name])
+@extends('layouts.app', ['title' => 'Manage Players '. $tournament->name])
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h2>Tournament Players</h2>
+            <h2>Manage Players</h2>
 
             <nav aria-label="breadcrumb" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('tournament', $tournament->id) }}">Tournament</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Tournament Players</li>
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('tournaments') }}">Tournaments</a></li>
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('tournament', $tournament->id) }}">{{ $tournament->name }}</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Manage Players</li>
                 </ol>
             </nav>
 
-            <h4>{{ $tournament->name }}</h4>
+            @if ($matches_scheduled > 0)
+            <div class="alert alert-info">
+                <strong>Matches are already scheduled!</strong> You have to empty all slots prior to adding or removing a player!
+            </div>
+            @endif
 
             @if ($errors->any())
                 @foreach ($errors->all() as $error)
@@ -29,30 +35,39 @@
 
     <div class="row justify-content-center mt-4">
         <div class="col-md-8">
+            <h3>{{ $tournament->name }}</h3>
+        </div>
+    </div>
+
+    <div class="row justify-content-center mt-4">
+        <div class="col-md-4">
             <h4>Invite a new player</h4>
-            
+
             <form method="post" action="{{ route('invite-player') }}">
                 @csrf
                 
                 <input type="hidden" name="tournament_id" value="{{ $tournament->id }}" />
 
                 <div class="mb-3">
-                    <label for="user_id" class="form-label">Name</label>
-                    <input class="form-control" type="text" name="name" value="{{ old('name') }}">
+                    <label for="user_id" class="form-label">Name <span class="text-danger">*</span></label>
+                    <input class="form-control" type="text" name="name" value="{{ old('name') }}" @if($matches_scheduled > 0) disabled @endif>
                 </div>
 
                 <div class="mb-3">
-                    <label for="user_id" class="form-label">Email</label>
-                    <input class="form-control" type="text" name="email" value="{{ old('email') }}">
+                    <label for="user_id" class="form-label">Email <span class="text-danger">*</span></label>
+                    <input class="form-control" type="text" name="email" value="{{ old('email') }}" @if($matches_scheduled > 0) disabled @endif>
                 </div>
 
-                <button type="submit" class="btn btn-primary">Invite</button>
+                <div class="mb-3">
+                    <label for="rating" class="form-label">Rating</label>
+                    <input class="form-control @error('rating') is-invalid @enderror" id="rating" name="rating" type="number" value="@if(old('rating')){{ old('rating') }}@endif" placeholder="Leave empty to let user decide rating">
+                </div>
+
+                <button type="submit" class="btn btn-primary" @if($matches_scheduled > 0) disabled @endif>Invite</button>
             </form>
         </div>
-    </div>
 
-    <div class="row justify-content-center mt-4">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <h4>Assign an existing player</h4>
             
             <form method="post" action="{{ route('assign-player') }}">
@@ -61,8 +76,8 @@
                 <input type="hidden" name="tournament_id" value="{{ $tournament->id }}" />
 
                 <div class="mb-3">
-                    <label for="user_id" class="form-label">Name</label>
-                    <select class="form-select" id="user_id" name="user_id" @if (count($users) == 0) disabled @endif>
+                    <label for="user_id" class="form-label">Name <span class="text-danger">*</span></label>
+                    <select class="form-select" id="user_id" name="user_id" @if (count($users) == 0 || $matches_scheduled > 0) disabled @endif>
                         @if (count($users) == 0)
                         <option value="">All users are assigned already</option>
                         @else    
@@ -70,28 +85,33 @@
                         @endif
 
                         @foreach ($users as $user)
-                            <option value="{{ $user->id }}">{{ $user->name }} ({{ $user->email }})</option>
+                            <option value="{{ $user->id }}">{{ $user->name }} ({{ $user->rating }})</option>
                         @endforeach
                     </select>
                 </div>
 
                 <div class="mb-3">
+                    <label for="rating" class="form-label">Rating</label>
+                    <input class="form-control @error('rating') is-invalid @enderror" id="rating" name="rating" type="number" value="@if(old('rating')){{ old('rating') }}@endif" placeholder="Leave empty to use user' rating">
+                </div>
+
+                <div class="mb-3">
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" value="1" name="clinic" id="clinic">
+                        <input class="form-check-input" type="checkbox" value="1" name="clinic" id="clinic" @if($matches_scheduled > 0) disabled @endif>
                         <label class="form-check-label" for="clinic">
-                            Clinic
+                            Join clinic
                         </label>
                     </div>
                 </div>
 
-                <button type="submit" class="btn btn-primary" @if (count($users) == 0) disabled @endif>Assign</button>
+                <button type="submit" class="btn btn-primary" @if(count($users) == 0 || ($matches_scheduled > 0)) disabled @endif>Assign</button>
             </form>
         </div>
     </div>
 
     <div class="row justify-content-center mt-4">
         <div class="col-md-8">
-            <h4>Assigned Players</h4>
+            <h4>Enrolled players</h4>
                             
             @if (count($tournament_players) == 0)
                 <p>There are no players assigned yet.</p>
@@ -100,12 +120,12 @@
             <ul class="list-group">
                 @foreach ($tournament_players as $player)
                     <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <div class="ms-2 me-auto">
-                            {{ $player->name }}
+                        <div class="ms-2 me-auto" id="player{{ $player->id }}">
+                            {{ $player->name }} ({{ $player->rating }})
                         </div>
                         
                         @if ($player->clinic == 1)
-                            <span class="badge rounded-pill text-bg-primary mt-1">Clinic</span>
+                            <span class="badge rounded-pill bg-info mt-1">Clinic</span>
                         @endif
 
                         @if ($player->present == 0)
@@ -118,7 +138,7 @@
                             <input type="hidden" name="id" value="{{ $player->id }}" />
 
                             @if ($player->present == 0)
-                            <button type="submit" name="submit" class="btn btn-success ms-2" style="--bs-btn-padding-y: 0.1rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">Mark present</button>
+                            <button type="submit" name="submit" class="btn btn-success ms-2" style="--bs-btn-padding-y: 0.1rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; color: #fff">Mark present</button>
                             @else
                             <button type="submit" name="submit" class="btn btn-warning ms-2" style="--bs-btn-padding-y: 0.1rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">Mark absent</button>
                             @endif
@@ -130,7 +150,7 @@
                             <input type="hidden" name="id" value="{{ $player->id }}" />
                             <input type="hidden" name="name" value="{{ $player->name }}" />
 
-                            <button type="submit" name="submit" class="btn btn-danger ms-2" style="--bs-btn-padding-y: 0.1rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">Remove</button>
+                            <button type="submit" name="submit" class="btn btn-danger ms-2" style="--bs-btn-padding-y: 0.1rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" @if($matches_scheduled > 0) disabled @endif>Remove</button>
                         </form>
                     </li>
                 @endforeach
