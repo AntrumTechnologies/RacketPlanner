3d5f7950885b6ea6c9106721efb8027ed60e1a51
Patrick van Broeckhuijsen <github@patb.ro>
2023-11-19 19:42:44 +0100

    Add breadcrumbs

--- resources/views/organization.blade.php
+++ resources/views/organization.blade.php
@@ -6,6 +6,14 @@
         <div class="col-md-8">
             <h2>{{ $organization->name }}</h2>
 
+            <nav aria-label="breadcrumb" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('organizations') }}">Organizations</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">{{ $organization->name }}</li>
+                </ol>
+            </nav>
+
             <!-- TODO(PATBRO): Perhaps add a contact button? Add edit button if this is your organization -->
         </div>
     </div>
--- resources/views/tournaments.blade.php
+++ resources/views/tournaments.blade.php
@@ -6,6 +6,13 @@
         <div class="col-md-8">
             <h2>Tournaments</h2>
 
+            <nav aria-label="breadcrumb" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Tournaments</li>
+                </ol>
+            </nav>
+
             @if (count($tournaments) == 1)
             <p>There is 1 tournament to show.</p>
             @else
