a74e62c9bb8d4c2dcd47c3d4e28140b74baa2da5
Patrick van Broeckhuijsen <github@patb.ro>
2023-11-26 17:36:34 +0100

    Add bookkeeping changes to tournament

---
+++ database/migrations/2023_11_25_075058_add_change_to_tournaments.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tournaments', function (Blueprint $table) {
+            $table->boolean('change_to_courts_rounds')->nullable();
+            $table->boolean('change_to_players')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tournaments', function (Blueprint $table) {
+            $table->dropColumn('change_to_courts_rounds');
+            $table->dropColumn('change_to_players');
+        });
+    }
+};
