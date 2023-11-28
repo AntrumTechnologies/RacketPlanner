e1e7fd08ea9bc63c1bee21af8d367407227ba42c
Patrick van Broeckhuijsen <github@patb.ro>
2023-11-28 19:42:18 +0100

    Remove name field autofocus

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,7 +18,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="@isset($name){{ $name }}@endisset" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="@isset($name){{ $name }}@endisset" required autocomplete="name">
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
