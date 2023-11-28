5746d0c167aa2ff088515056d35b34f4101915f1
Patrick van Broeckhuijsen <github@patb.ro>
2023-11-28 19:41:57 +0100

    Add breadcrumbs, remove card style, add cancel button

--- resources/views/admin/round.blade.php
+++ resources/views/admin/round.blade.php
@@ -6,6 +6,16 @@
         <div class="col-md-8">
             <h2>Edit Round</h2>
 
+            <nav aria-label="breadcrumb" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('tournaments') }}">Tournaments</a></li>
+                    <li class="breadcrumb-item" aria-current="page">Tournament</li>
+                    <li class="breadcrumb-item" aria-current="page">Manage Courts and Rounds</li>
+                    <li class="breadcrumb-item active" aria-current="page">Edit round</li>
+                </ol>
+            </nav>
+
             @if ($errors->any())
                 @foreach ($errors->all() as $error)
                 <div class="alert alert-danger">{{ $error }}</div>
@@ -20,41 +30,25 @@
 
     <div class="row justify-content-center mt-4">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header d-flex">
-                    <div class="me-auto">
-                        {{ $round->name }}
-                    </div>
+            <h4>Round details</h4>
+            <form method="post" action="{{ route('update-round') }}">
+                @csrf
+                
+                <input type="hidden" name="id" value="{{ $round->id }}" />
+
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name <span class="text-danger">*</span></label>
+                    <input class="form-control @error('name') is-invalid @enderror" id="name" name="name" type="text" value="@if(old('name')){{ old('name') }}@else{{ $round->name }}@endif">
                 </div>
 
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <form method="post" action="{{ route('update-round') }}">
-                                @csrf
-                                
-                                <input type="hidden" name="id" value="{{ $round->id }}" />
-
-                                <div class="mb-3">
-                                    <label for="name" class="form-label">Name</label>
-                                    <input class="form-control @error('name') is-invalid @enderror" id="name" name="name" type="text" value="@if(old('name')){{ old('name') }}@else{{ $round->name }}@endif">
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="starttime" class="form-label">Start time</label>
-                                    <input class="form-control @error('starttime') is-invalid @enderror" id="starttime" name="starttime" type="text" value="@if(old('starttime')){{ old('starttime') }}@else{{ $round->starttime }}@endif" placeholder="Y-m-d H:i">
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="endtime" class="form-label">End time</label>
-                                    <input class="form-control @error('endtime') is-invalid @enderror" id="endtime" name="endtime" type="text" value="@if(old('endtime')){{ old('endtime') }}@else{{ $round->endtime }}@endif" placeholder="Y-m-d H:i">
-                                </div>
-
-                                <button type="submit" class="btn btn-primary">Update</button>
-                            </form>
-                        </div>
-                    </div>
+                <div class="mb-3">
+                    <label for="starttime" class="form-label">Start time <span class="text-danger">*</span></label>
+                    <input class="form-control @error('starttime') is-invalid @enderror" id="starttime" name="starttime" type="text" value="@if(old('starttime')){{ old('starttime') }}@else{{ $round->starttime }}@endif" placeholder="Y-m-d H:i">
                 </div>
+
+                <button type="submit" class="btn btn-primary">Update</button>
+                <a href="{{ url()->previous() }}" class="btn btn-danger">Cancel</a>
+            </form>
             </div>
         </div>
     </div>
