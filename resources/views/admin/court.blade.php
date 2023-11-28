8d6924308c78f6c98ee903ce5c6df927a895ca85
Patrick van Broeckhuijsen <github@patb.ro>
2023-11-28 19:39:27 +0100

    Add breadcrumbs, remove card style

--- resources/views/admin/court.blade.php
+++ resources/views/admin/court.blade.php
@@ -6,6 +6,16 @@
         <div class="col-md-8">
             <h2>Edit Court</h2>
 
+            <nav aria-label="breadcrumb" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item" aria-current="page"><a href="{{ route('tournaments') }}">Tournaments</a></li>
+                    <li class="breadcrumb-item" aria-current="page">Tournament</li>
+                    <li class="breadcrumb-item" aria-current="page">Manage Courts and Rounds</li>
+                    <li class="breadcrumb-item active" aria-current="page">Edit court</li>
+                </ol>
+            </nav>
+
             @if ($errors->any())
                 @foreach ($errors->all() as $error)
                 <div class="alert alert-danger">{{ $error }}</div>
@@ -20,32 +30,20 @@
 
     <div class="row justify-content-center mt-4">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header d-flex">
-                    <div class="me-auto">
-                        {{ $court->name }}
-                    </div>
+            <h4>Court details</h4>
+            <form method="post" action="{{ route('update-court') }}">
+                @csrf
+                
+                <input type="hidden" name="id" value="{{ $court->id }}" />
+
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name <span class="text-danger">*</span></label>
+                    <input class="form-control @error('name') is-invalid @enderror" id="name" name="name" type="text" value="@if(old('name')){{ old('name') }}@else{{ $court->name }}@endif">
                 </div>
 
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <form method="post" action="{{ route('update-court') }}">
-                                @csrf
-                                
-                                <input type="hidden" name="id" value="{{ $court->id }}" />
-
-                                <div class="mb-3">
-                                    <label for="name" class="form-label">Name</label>
-                                    <input class="form-control @error('name') is-invalid @enderror" id="name" name="name" type="text" value="@if(old('name')){{ old('name') }}@else{{ $court->name }}@endif">
-                                </div>
-
-                                <button type="submit" class="btn btn-primary">Update</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                <button type="submit" class="btn btn-primary">Update</button> 
+                <a href="{{ url()->previous() }}" class="btn btn-danger">Cancel</a>
+            </form>
         </div>
     </div>
 </div>
