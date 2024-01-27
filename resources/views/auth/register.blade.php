c65d840419e69cc2677e5146a32145e8794c5978
Patrick van Broeckhuijsen <github@patb.ro>
2024-01-27 09:55:24 +0100

    Improve register page with errors and static email field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
@@ -32,8 +36,8 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
-                                @if (isset($email))
-                                    <input id="email" type="email" class="form-control-plaintext" name="email" value="{{ $email }}" readonly>
+                                @if (isset($email) || !empty(Request::get('email')))
+                                    <input id="email" type="email" class="form-control-plaintext" name="email" value="@isset($email){{ $email }}@else{{ Request::get('email') }}@endisset" readonly>
                                 @else
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" required autocomplete="email">
                                 @endif
